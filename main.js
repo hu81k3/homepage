bcf79a812cc80e54f9f024120fd754bdd44231a2
Hubert Kruk <hu81k3@gmail.com>
2020-06-23 20:45:13 +0200

    Added main headings on subpages.

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 const $name = "Hubert";
 console.log("Cześć, jestem " + $name + ", a Ty jak masz na imię?");
+
+const additional = document.querySelector('.author__description--js');
+additional.innerHTML = 'Tutaj jest innerHTML wrzucony poprzez JS. W ten sposób wykonałem jedno z zadań domowych.';
