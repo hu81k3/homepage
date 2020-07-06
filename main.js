db96645982720ef405059c26fd4915e4038d5c45
Hubert Kruk <hu81k3@gmail.com>
2020-07-06 22:33:13 +0200

    Done a bit of homework.

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const $name = "Hubert";
-console.log("Cześć, jestem " + $name + ", a Ty jak masz na imię?");
+const name = "Hubert";
+const age = 22;
 
 const additional = document.querySelector('.author__description--js');
 additional.innerHTML = 'Tutaj jest innerHTML wrzucony poprzez JS. W ten sposób wykonałem jedno z zadań domowych.';
+
+// Zadania z tygodnia V
+
+function greet(myNumber) {
+    console.log(`Hej! Mam na imię ${name}. Mam ${age} lata i chcę zostać fullstack developerem. 😊`);
+}
+
+const myGreetings = greet(name, age);
+
+console.log(age);
